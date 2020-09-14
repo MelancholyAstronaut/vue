d3123d740af08355358f53f2cdb8ad49b76f0cb2
不愿意透露姓名的某某某 <54980372+MelancholyAstronaut@users.noreply.github.com>
2020-09-14 17:35:23 +0800

    vue

---
+++ slot/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>My Todo App!</h1>
+    <h1>My Todo App!</h1>
+    <h1>My Todo App!</h1>
+    <h1>My Todo App!</h1>
+  </div>
+  <slot></slot>
+</template>
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    entry: './slot/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test:/\.vue$/,
+                use:['vue-loader']
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin!
+        new VueLoaderPlugin()
+    ]
+};
